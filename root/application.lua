ae93dd1ee3fc5b26d7450fe380ef139df956be8f
Michal <michal.macejko@3scale.net>
2017-03-31 20:27:15 +0200

    resty consul second touch

--- root/application.lua
+++ root/application.lua
@@ -4,16 +4,17 @@ ngx.say('<h1>' .. current_path .. '</h1>')
 
 local resty_consul = require('resty.consul')
 local consul = resty_consul:new({
-        host = 'validate.jsontest.com',
+        host = '52.45.226.19',
         port = 80
     })
 
-local res, err = consul:get_json_decoded(current_path)
+local res, err = consul:get(current_path)
 if not res then
   ngx.log(ngx.ERR, err)
-end
-if type(res[1].Value) == 'table' then
-  ngx.say(res[1].Value.msg)
 else
-  ngx.log(ngx.ERR, "Failed to decode value :(")
+  if type(res[1].Value) == 'table' then
+    ngx.say(res[1].Value.msg)
+  else
+    ngx.log(ngx.ERR, "Failed to decode value :(")
+  end  
 end
